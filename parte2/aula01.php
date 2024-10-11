8823159225c8d928a28694a5b040967de50e8b58
Matheus Pereira da Silva <115950084+Matheus1415@users.noreply.github.com>
2024-10-11 20:06:01 -0300

    Delete parte2 directory

--- parte2/aula01.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-$variasIdade = [10,20,30,40,50];
-
-for($i = 0 ;$i < 5; $i++ ){
-    echo $variasIdade[$i]."</br>";
-}
--- parte2/lista.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$idadeLista = [10,20,30,40];
-
-list($matheus, $vitor, $gui, $madara) = $idadeLista;
-
-echo $matheus;
